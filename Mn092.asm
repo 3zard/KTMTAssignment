9dfe2e2564a7be81f6158fab0b2754de78edfb2e
3zard <trinh.nguyen3zard@hcmut.edu.vn>
2024-11-19 09:39:19 +0700

    Terminal Trình | Test read/write files

--- Mn092.asm
+++ Mn092.asm
@@ -4,7 +4,6 @@
 	.data
 #Cac dinh nghia bien
 filename: .asciiz  "FLOAT2.BIN"
-.align 2
 buffer: .space 4			# Cung cấp buffer để đọc dữ liệu từ file
 fdescr: .word 0			# Lưu trữ file descriptor
 float1: .float 1.25 		# Số thực thứ 1
@@ -21,12 +20,16 @@ main:
 #Mở file
 	la $a0, filename			# Đưa địa chỉ filename vào $a0
 	addi $a1, $zero, 0		# Chế độ đọc
+	addi $a2, $zero, 2 		# Chế độ đọc-ghi
 	addi $v0, $zero, 13		# Syscall 13: Open file
 	syscall				# Thực hiện syscall mở file
 	bgez $v0, tiep
 bao_loi:
 	la $a0, str_loi
 	addi $v0, $zero, 4
+	syscall
+	li $v0, 1                 		# Syscall 1: Print integer (Xem mã lỗi)
+	move $a0, $v0             	# Đưa kết quả mở file vào $a0 để in
 	syscall
 	j Kthuc
 tiep:	sw $v0, fdescr			# Lưu file descriptor
@@ -37,6 +40,13 @@ tiep:	sw $v0, fdescr			# Lưu file descriptor
 	li $a2, 4				# Đọc tối đa 4 byte
 	li $v0, 14				# Syscall 14: Read file
 	syscall
+
+# Kiểm tra giá trị trong buffer
+	lw $s1, buffer       # Đưa dữ liệu từ buffer vào $s1
+	mtc1 $s1, $f12       # Chuyển giá trị từ $s1 vào $f12 để in ra
+	li $v0, 2            # Syscall 2: Print float
+	syscall
+
 
 # Đóng file
 	lw $a0, fdescr			#Load file descriptor
