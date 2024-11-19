4d68d211a39123682ab0b60ccc7cf60bd0dc6021
3zard <trinh.nguyen3zard@hcmut.edu.vn>
2024-11-19 09:14:32 +0700

    Trình | Test read/write files

--- Mn092.asm
+++ Mn092.asm
@@ -19,10 +19,9 @@ str_loi: .asciiz "Sai"
 	.text
 main:
 #Mở file
-	la $t1, buffer	
 	la $a0, filename			# Đưa địa chỉ filename vào $a0
-	li $a1, 0				# Chế độ đọc
-	li $v0, 13				# Syscall 13: Open file
+	addi $a1, $zero, 0		# Chế độ đọc
+	addi $v0, $zero, 13		# Syscall 13: Open file
 	syscall				# Thực hiện syscall mở file
 	bgez $v0, tiep
 bao_loi:
@@ -46,7 +45,7 @@ tiep:	sw $v0, fdescr			# Lưu file descriptor
 
 #In ra số thực thứ 1
 	lw $s1, buffer            	# Đưa dữ liệu từ buffer vào $s1
-	mtc1 $t1, $f12       		# Di chuyển dữ liệu từ $s1 vào $f12 để in ra
+	mtc1 $s1, $f12       		# Di chuyển dữ liệu từ $s1 vào $f12 để in ra
 	li $v0, 2           			# Syscall 2: Print float	
 	syscall             			# Thực hiện syscall để in
 
