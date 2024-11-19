506672dff47cf6f5ef21f5328a5c147f57fbf27e
3zard <trinh.nguyen3zard@hcmut.edu.vn>
2024-11-19 09:02:39 +0700

    Trình | Test read/write files

--- Mn092.asm
+++ Mn092.asm
@@ -4,10 +4,11 @@
 	.data
 #Cac dinh nghia bien
 filename: .asciiz  "FLOAT2.BIN"
-buffer: .word 0			# Cung cấp buffer để đọc dữ liệu từ file
-file_descriptor: .word 0	# Lưu trữ file descriptor
-float1: .word 0x3F800000 # Số thực thứ 1
-float2: .word 0x40000000 # Số thực thứ 2
+.align 2
+buffer: .space 4			# Cung cấp buffer để đọc dữ liệu từ file
+fdescr: .word 0			# Lưu trữ file descriptor
+float1: .float 1.25 		# Số thực thứ 1
+float2: .float 2.0 		# Số thực thứ 2
 #Cac cau nhac nhap du lieu
 xuat_f1: .asciiz "So thuc thu nhat la: "
 xuat_f2: .asciiz "So thuc thu hai la: "
@@ -18,33 +19,32 @@ line_break: .asciiz "/n"
 main:
 #Mở file
 	la $t1, buffer	
-	li $v0, 13			# Syscall 13: Open file
-	la $a0, filename		# Đưa địa chỉ filename vào $a0
-	li $a1, 0			# Chế độ đọc
-	li $a2, 2			# Chế độ đọc-ghi
-	syscall			# Thực hiện syscall mở file
-
-# Lưu file descriptor vào file_descriptor
-	move $s0, $v0      	# Lưu kết quả trả về vào $s0
-	
+	la $a0, filename			# Đưa địa chỉ filename vào $a0
+	li $a1, 0				# Chế độ đọc
+	li $v0, 13				# Syscall 13: Open file
+	syscall				# Thực hiện syscall mở file
+	bgez $v0, tiep
+bao_loi:
+	j Kthuc
+tiep:	sw $v0, fdescr			# Lưu file descriptor
 #Đọc file
 #Đọc số thực thứ 1
-	li $v0, 14				# Syscall 14: Read file
-	move $a0, $s0			# Sử dụng file descriptor đã mở
-	la $a1, 0($t1)			# Địa chỉ buffer để lưu dữ liệu đọc được
+	lw $a0, fdescr			# Load file descriptor
+	la $a1, buffer			# lấy địa chỉ buffer
 	li $a2, 4				# Đọc tối đa 4 byte
-	syscall				# Thực hiện syscall đọc file
-	lw $s1, 0($t1)
-
-#In ra số thực thứ 1
-	li $v0, 1           # Syscall 1: Print integer
-	move $a0, $s1       # Di chuyển dữ liệu từ $s1 vào $a0 để in ra
-	syscall             # Thực hiện syscall để in
+	li $v0, 14				# Syscall 14: Read file
+	syscall
 
 # Đóng file
-    li $v0, 16         # Syscall 16: Close file
-    move $a0, $s0      # Sử dụng file descriptor đã mở
-    syscall            # Thực hiện syscall để đóng file
+	lw $a0, fdescr			#Load file descriptor
+    	li $v0, 16         			# Syscall 16: Close file
+    	syscall            			# Thực hiện syscall để đóng file
+
+#In ra số thực thứ 1
+	lw $s1, buffer            	# Đưa dữ liệu từ buffer vào $s1
+	mtc1 $t1, $f12       		# Di chuyển dữ liệu từ $s1 vào $f12 để in ra
+	li $v0, 2           			# Syscall 2: Print float	
+	syscall             			# Thực hiện syscall để in
 
 #Nhap (syscall)
 #Xu ly
